29e052bb9ececa0e38958764b63ec67a4bcd2335
mrnegativex <rashadul99924@gmail.com>
2022-05-29 07:17:13 +0600

    make admin funcionality added

--- src/Components/Genarel/Dashboard/Admin/MakeAdmin.js
+++ src/Components/Genarel/Dashboard/Admin/MakeAdmin.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { signOut } from "firebase/auth";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -8,7 +9,7 @@ import Loading from "../../Shared/Loading";
 const MakeAdmin = () => {
   const [user] = useAuthState(auth);
   const email = user?.email;
-  const { data, isLoading, refetch } = useQuery("myorder", () =>
+  const { data, isLoading, refetch } = useQuery("makeAdmin", () =>
     fetch(`http://localhost:5000/users`, {
       headers: {
         authorization: `Bearer ${localStorage.getItem("accessToken")}`,
@@ -22,21 +23,31 @@ const MakeAdmin = () => {
     signOut(auth);
     return window.location.reload();
   }
+
+  // make admin
+  const makeAdmin = (cEmail) => {
+      console.log(cEmail);
+    axios.put(`http://localhost:5000/users/role/${cEmail}`)
+      .then((response) => {
+        console.log(response);
+        const { data } = response;
+      });
+  };
+
   return (
     <div>
       <div>
-        <h1 className="text-5xl uppercase font-bold text-center my-5">
-          MY Order
+        <h1 className="text-5xl uppercase font-bold text-center my-7">
+          Make Admin
         </h1>
         <div className="overflow-x-auto lg:px-10 md:px-5 px1">
-          <table className="table table-zebra w-full">
+          <table className="table text-center table-zebra w-full">
             <thead>
               <tr>
                 <th>#</th>
                 <th>Email</th>
                 <th>Make Admin</th>
                 <th>Delete User</th>
-                
               </tr>
             </thead>
             <tbody>
@@ -44,7 +55,16 @@ const MakeAdmin = () => {
                 <tr key={user._id}>
                   <th>1</th>
                   <td>{user.email}</td>
-                  
+                  <td>
+                    {
+                        user.role ? 'Admin' : <button
+                        onClick={() => makeAdmin(user.email)}
+                        className="btn btn-sm"
+                      >
+                        Make Admin
+                      </button>
+                    }
+                  </td>
                 </tr>
               ))}
             </tbody>
